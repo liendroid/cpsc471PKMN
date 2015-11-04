9aba9bf18237e950881d20efef3c576df6fc16a1
SARTAJ HUNDAL <vektor.knight@gmail.com>
2015-11-04 13:08:10 -0700

    Update LogIn.php

--- LogIn.php
+++ LogIn.php
@@ -1,11 +1,13 @@
+<?php
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <?php
-        //stick our php logic in here. 
-        ?>
+    <script type="text/javascript"> 
+        dynamics.animate(document.querySelector('.circle'), { translateX: 350 }, { type: dynamics.linear })
+    </script>
         <h1>Login Successful for the time being. Add stuff with database later</h1>
     </body>
 </html>
+?>
